f4c61c993c3f41b34daf86b6a617b876d03d97bd
Angus Williams <anguswilliams91@gmail.com>
2020-08-23 11:38:10 +0100

    WIP: adding action to produce odds table

---
+++ .github/make_predictions.py
@@ -0,0 +1,148 @@
+"""Produce predictions for premier league matches occurring this week."""
+import argparse
+import datetime
+import requests
+
+import bpl
+import pandas as pd
+import tabulate
+from urllib.error import HTTPError
+
+
+def download_data(now):
+    """Download this season and last season's data for training."""
+    this_year = int(str(now.year)[2:])
+
+    if now.month >= 8:
+        this_season = f"{this_year}{this_year + 1}"
+        last_season = f"{this_year -1}{this_year}"
+    else:
+        this_season = f"{this_year - 1}{this_year}"
+        last_season = f"{this_year - 2}{this_year - 1}"
+
+    data_url = lambda season: f"https://www.football-data.co.uk/mmz4281/{season}/E0.csv"
+    this_season_url = data_url(this_season)
+    last_season_url = data_url(last_season)
+
+    last_season_results = pd.read_csv(last_season_url)
+    try:
+        this_season_results = pd.read_csv(this_season_url)
+    except HTTPError:
+        this_season_results = None
+
+    results = pd.concat((last_season_results, this_season_results))
+    results = results[["HomeTeam", "AwayTeam", "FTHG", "FTAG", "Date"]].rename(
+        {
+            "Date": "date",
+            "HomeTeam": "home_team",
+            "AwayTeam": "away_team",
+            "FTHG": "home_goals",
+            "FTAG": "away_goals",
+        },
+        axis=1,
+    )
+
+    results.to_csv("training_set.csv", index=False)
+
+
+def get_fixtures_for_week(now):
+    """Use the fantasy football API to get the fixtures that are coming up this week."""
+    url = "https://fantasy.premierleague.com/api/fixtures/"
+    fixtures = requests.get(url).json()
+
+    this_week_fixtures = []
+    for fixture in fixtures:
+        kickoff_time = fixture["kickoff_time"]
+        if not kickoff_time:
+            continue
+        elif (
+            pd.to_datetime(fixture["kickoff_time"]).date()
+            < (now + datetime.timedelta(days=7)).date()
+        ) and (pd.to_datetime(fixture["kickoff_time"]).date() >= now.date()):
+            this_week_fixtures.append(fixture)
+
+    teams_url = "https://fantasy.premierleague.com/api/bootstrap-static/"
+    teams = requests.get(teams_url).json()["teams"]
+
+    code_to_team = {team["id"]: team["name"] for team in teams}
+
+    matches = [
+        {
+            "date": pd.to_datetime(fixture["kickoff_time"]).date(),
+            "home_team": code_to_team[fixture["team_h"]],
+            "away_team": code_to_team[fixture["team_a"]],
+        }
+        for fixture in this_week_fixtures
+    ]
+
+    if not matches:
+        return
+    else:
+        pd.DataFrame(matches).set_index("date").to_csv("fixtures.csv")
+
+
+def make_predictions():
+    """Make predictions for this gameweek."""
+
+    output_path = f"predictions_{datetime.datetime.now().date()}.md"
+
+    try:
+        fixtures = pd.read_csv("fixtures.csv")
+    except FileNotFoundError:
+        # no matches, so just write empty string
+        markdown_table = ""
+        with open(output_path, "w") as f:
+            f.write(markdown_table)
+        return
+
+    # fit the model
+    training_set = pd.read_csv("training_set.csv")
+    model = bpl.BPLModel(training_set)
+    model.fit()
+
+    # add missing teams if necessary (first GW of season for promoted teams)
+    teams = set(fixtures["home_team"]) | set(fixtures["away_team"])
+    model_teams = set(model.team_indices.keys())
+    missing_teams = teams - model_teams
+    for team in missing_teams:
+        model.add_new_team(team)
+
+    # make predictions
+    predictions = model.predict_future_matches(fixtures)
+    predictions = predictions.rename(
+        {
+            "pr_home": "Home win",
+            "pr_away": "Away win",
+            "pr_draw": "Draw",
+            "date": "Date",
+            "home_team": "Home team",
+            "away_team": "Away team",
+        },
+        axis=1,
+    ).round(decimals=2)
+
+    markdown_table = tabulate.tabulate(
+        predictions.values, headers=predictions.columns, tablefmt="pretty"
+    )
+    print(markdown_table)
+    with open(output_path, "w") as f:
+        f.write(markdown_table)
+
+
+def main(date):
+    download_data(date)
+    get_fixtures_for_week(date)
+    make_predictions()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--date", dest="date", type=str, default="")
+    args = parser.parse_args()
+
+    if not args.date:
+        date = datetime.datetime.now()
+    else:
+        date = pd.to_datetime(args.date)
+
+    main(date)
